63ec69e6fc6ba1519b0c60bc3b93c500f245468e
Ebu13 <ebubekir13042003@gmail.com>
2025-07-12 13:00:07 +0300

    last

--- proje-kod-ve-raporu-Ebu13/bekommerce_bot/config.py
+++
@@ -1,7 +0,0 @@
-DB_CONFIG = {
-    'dbname': 'bekommerce',
-    'user': 'postgres',
-    'password': '13042003',
-    'host': 'db',
-    'port': '5432'
-}
--- proje-kod-ve-raporu-Ebu13/client/src/utils/formatDate.js
+++
@@ -1,8 +0,0 @@
-export const formatDate = (date) => {
-    return new Date(date).toLocaleDateString();
-  };
-
-/*
-Yardımcı fonksiyonlar burada tutulur.
-Örneğin, tarih formatlama veya para birimi dönüştürme fonksiyonları burada olur.
-*/
